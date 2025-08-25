898331727cf2eb06baa56a15251d23918f9ddd37
Anjalimalviya1234 <Anjalimalviya719@gmail.com>
2025-08-25 12:32:22 -0700

    Initial commit:URL Shortener project setup

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  avatar: {
+    type: String,
+    required: false,
+    default: "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp",
+  },
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+
+
+    // Ensure that both candidatePassword and the stored password are defined
+    if (!candidatePassword) {
+        throw new Error("Missing password or invalid password");
+    }
+      
+    return await bcrypt.compare(candidatePassword, this.password);
+
+
+
+
+
+    
+};
+
+userSchema.set('toJSON', {
+  transform: function (doc, ret) {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
